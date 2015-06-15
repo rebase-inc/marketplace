0fd3a1087e8e8a5adc9259c7acb79bcb6d29c688
Andrew Millspaugh <andrewmillspaugh@Andrews-MacBook.local>
2015-06-15 14:35:47 -0700

    Got some simple react functionality working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,91 @@
-var MainContent = React.createClass({
+var FindTalentIcon = React.createClass({
+    render: function() {
+        return (
+            <svg className='findTalentIcon' width="18px" height="25px" viewBox="0 0 18 25">
+            <g id="UI" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd">
+            <g id="Mgr-New-Tickets" transform="translate(-374.000000, -116.000000)" fill="#BDBFBF">
+            <g id="Long-Ticket" transform="translate(325.000000, 70.500000)">
+            <g id="Fill-711" transform="translate(49.500000, 46.243000)">
+            <path d="M10.7344,0.0498 C10.5184,-0.0472 10.2624,0.0228 10.1234,0.2158 L0.1234,14.2158 C0.0154,14.3678 0.0004,14.5688 0.0864,14.7358 C0.1714,14.9018 0.3434,15.0068 0.5304,15.0068 L7.9074,15.0068 L6.0424,23.3978 C5.9914,23.6298 6.1104,23.8658 6.3274,23.9628 C6.3934,23.9918 6.4624,24.0068 6.5304,24.0068 C6.6884,24.0068 6.8414,23.9318 6.9374,23.7968 L16.9374,9.7968 C17.0464,9.6448 17.0614,9.4448 16.9754,9.2778 C16.8894,9.1118 16.7184,9.0068 16.5304,9.0068 L9.1544,9.0068 L11.0184,0.6148 C11.0704,0.3828 10.9514,0.1468 10.7344,0.0498"></path>
+            </g>
+            </g>
+            </g>
+            </g>
+            </svg>
+        );
+    }
+});
+
+var CommentIcon = React.createClass({
+    render: function() {
+        return (
+            <svg className='commentIcon' width="25px" height="24px" viewBox="0 0 25 24">
+            <g id="UI" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd">
+            <g transform="translate(-1354.000000, -117.000000)" fill="#CBD0D4">
+            <g transform="translate(325.000000, 70.500000)">
+            <g transform="translate(1029.000000, 46.243000)">
+            <path d="M17.0308,9.0068 C17.5068,9.0068 17.9728,9.0518 18.4298,9.1218 C18.4948,8.7428 18.5308,8.3498 18.5308,7.9418 C18.5308,3.8418 14.4938,0.5068 9.5308,0.5068 C4.5678,0.5068 0.5308,3.8418 0.5308,7.9418 C0.5308,9.9028 1.3858,11.7678 2.8908,13.1188 L1.0968,16.2588 C0.9908,16.4438 1.0138,16.6738 1.1538,16.8348 C1.2508,16.9458 1.3888,17.0068 1.5308,17.0068 C1.5928,17.0068 1.6568,16.9948 1.7168,16.9708 L6.5558,15.0348 C6.9908,15.1588 7.4428,15.2528 7.9028,15.3158 C7.9258,15.3188 7.9678,15.3118 7.9788,15.3198 C8.0538,15.3198 8.2978,15.3178 8.6568,15.2978 C9.3358,11.7838 12.8898,9.0068 17.0308,9.0068" id="Fill-1121"></path>
+            <path d="M17.0308,10.0068 C12.9658,10.0068 9.5308,12.9838 9.5308,16.5068 C9.5308,19.8628 12.4088,22.7308 15.9648,23.0798 C15.9668,23.0798 15.9688,23.0798 15.9708,23.0808 C16.1198,23.0948 16.2728,23.0968 16.4248,23.1028 C16.5138,23.1058 16.6008,23.1148 16.6908,23.1148 L16.7068,23.1148 C16.9378,23.1138 17.1678,23.1028 17.3968,23.0808 C17.4808,23.0728 17.5638,23.0568 17.6478,23.0458 C17.7978,23.0268 17.9468,23.0078 18.0948,22.9788 C18.1898,22.9608 18.2838,22.9348 18.3788,22.9128 C18.5138,22.8808 18.6498,22.8498 18.7838,22.8108 C18.8798,22.7818 18.9748,22.7468 19.0708,22.7148 C19.2288,22.6608 19.3878,22.6098 19.5428,22.5458 L19.7238,22.6128 L23.3558,23.9748 C23.4128,23.9968 23.4718,24.0068 23.5308,24.0068 C23.6698,24.0068 23.8058,23.9488 23.9028,23.8408 C24.0398,23.6888 24.0698,23.4668 23.9778,23.2838 L22.8188,20.9648 L22.6528,20.6338 C22.7788,20.5208 22.8938,20.4018 23.0078,20.2818 C23.0618,20.2258 23.1138,20.1678 23.1648,20.1098 C23.2478,20.0138 23.3288,19.9178 23.4048,19.8188 C23.4398,19.7738 23.4748,19.7278 23.5078,19.6818 C23.5948,19.5618 23.6768,19.4398 23.7518,19.3148 C23.7698,19.2848 23.7888,19.2558 23.8058,19.2258 C23.8898,19.0818 23.9668,18.9348 24.0368,18.7848 C24.0438,18.7688 24.0518,18.7528 24.0598,18.7368 C24.1318,18.5758 24.1968,18.4108 24.2528,18.2438 C24.2558,18.2338 24.2598,18.2238 24.2628,18.2138 C24.3178,18.0468 24.3638,17.8758 24.4018,17.7018 C24.4048,17.6878 24.4088,17.6748 24.4118,17.6608 C24.4468,17.4968 24.4718,17.3288 24.4908,17.1588 C24.4938,17.1348 24.4988,17.1118 24.5008,17.0878 C24.5208,16.8968 24.5308,16.7028 24.5308,16.5068 C24.5308,12.9838 21.0958,10.0068 17.0308,10.0068" id="Fill-1122"></path>
+            </g>
+            </g>
+            </g>
+            </g>
+            </svg>
+        );
+    }
+});
+
+var DropdownIcon = React.createClass({
+    render: function() {
+        return (
+            <svg className='dropdownIcon' width="16px" height="10px" viewBox="0 0 22 14">
+            <g stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd">
+            <g transform="translate(-279.000000, -148.000000)" fill="#FFFFFF">
+            <g transform="translate(279.000000, 148.000000)">
+            <path d="M21.84644,2.34671111 L19.57428,0.625488889 C19.39036,0.485488889 19.11228,0.504933333 18.95388,0.667488889 L11.43868,8.41493333 L3.92612,0.667488889 C3.85044,0.588933333 3.7422,0.540711111 3.62604,0.532933333 C3.509,0.524377778 3.3946,0.5586 3.30572,0.625488889 L1.03356,2.34671111 C0.84876,2.48671111 0.82764,2.73171111 0.98604,2.89504444 L11.10428,13.3304889 C11.18788,13.4168222 11.3102,13.4666 11.43868,13.4666 C11.56716,13.4666 11.68948,13.4168222 11.77308,13.3304889 L21.89396,2.89504444 C21.96964,2.81648889 22.00748,2.7146 21.99868,2.61193333 C21.98988,2.50926667 21.93532,2.4136 21.84644,2.34671111"></path>
+            </g>
+            </g>
+            </g>
+            </svg>
+        );
+    }
+});
+
+var SidebarViewSelector = React.createClass({
+    render: function() {
+        return ( <div id='sidebarViewSelector'><span>alveare/api</span><DropdownIcon/></div>);
+    }
+});
+
+var NewTicketView = React.createClass({
+    getInitialState: function() { 
+        return { filterText: '' };
+    },
+    handleUserInput: function(filterText) {
+        this.setState({ filterText: filterText });
+    },
     render: function() {
         return (
             <div id='mainContent'>
-            <SearchBar />
-            <NewTicketList tickets={TICKETS}/>
+            <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput}/>
+            <NewTicketList tickets={this.props.tickets} filterText={this.state.filterText}/>
             </div>);
+    }
+});
+
+var FindTalentPanel = React.createClass({
+    handleClick: function() {
+       console.log(this);
+       //alert('Finding talent, motherfucker.');
+    }, 
+    render: function() {
+        return (
+            <td onClick={this.handleClick} className='findTalentPanel'>
+            <FindTalentIcon/>
+            <span>Find Talent</span>
+            </td>
+
+        );
     }
 });
 
@@ -12,13 +93,13 @@ var NewTicket = React.createClass({
     render: function() {
         return (
             <tr className='newTicket'>
-                <td className='findTalentPanel'>
-                    <object data='img/lightning-bolt-icon-24px.svg' type='image/svg+xml'></object>
-                    <p>Find Talent</p>
-                </td>
+                <FindTalentPanel />
                 <td className='titlePanel'>{this.props.ticket.title}</td>
                 <td className='skillsRequiredPanel'>{this.props.ticket.skillsRequired}</td>
-                <td className='commentsPanel'>{this.props.ticket.comments.length} Comments</td>
+                <td className='commentsPanel'>
+                    <CommentIcon/>
+                    <span>{this.props.ticket.comments.length} Comments</span>
+                </td>
             </tr>
         );
     }
@@ -28,8 +109,11 @@ var NewTicketList = React.createClass({
     render: function() {
         var all_tickets = [];
         this.props.tickets.forEach(function(ticket) {
+            if ( ticket.title.indexOf(this.props.filterText) == -1 ) { 
+                return; 
+            }
             all_tickets.push(<NewTicket ticket={ticket} key={ticket.date} />);
-        });
+        }.bind(this));
         return (
             <table id='newTicketList'>
             <tbody>{all_tickets}</tbody>
@@ -39,12 +123,48 @@ var NewTicketList = React.createClass({
 });
 
 var SearchBar = React.createClass({
+    handleChange: function() {
+        this.props.onUserInput( this.refs.filterTextInput.getDOMNode().value );
+    },
     render: function() {
         return (
                 <form id='newTicketSearchBar'>
-                    <input type='text' placeholder='Search for tickets'/>
+                    <input type='text' placeholder='Search for tickets' value={this.props.filterText} onChange={this.handleChange} ref='filterTextInput'/>
                 </form>
                );
+    }
+});
+
+
+var ViewSelection = React.createClass({
+    selectView: function() { 
+        this.props.changeView( this.props.view.name ); 
+    },
+    render: function() {
+        var className = 'viewSelection';
+        if (this.props.view.name == this.props.currentViewName) { className = className + ' selected'; }
+        return (
+            <div className={className} onClick={this.selectView}>
+            <span className='viewIcon'>
+            <img src={this.props.view.icon}/>
+            </span>
+            <span className='viewName'>{this.props.view.name}</span>
+            </div>
+        );
+    }
+});
+
+var ViewList = React.createClass({
+    render: function() {
+        all_views = [];
+        this.props.views.forEach(function(view) { 
+            all_views.push(<ViewSelection changeView={this.props.changeView} currentViewName={this.props.currentViewName} view={view}/>);
+        }.bind(this));
+        return (
+            <div id='viewList'>
+                {all_views}
+            </div>
+        );
     }
 });
 
@@ -52,10 +172,8 @@ var SidebarNav = React.createClass({
     render: function() {
         return (
             <div>
-                <div>Some random view</div>
-                <div>New/Waiting</div>
-                <div>In Progress</div>
-                <div>Completed</div>
+            <SidebarViewSelector/>
+            <ViewList currentViewName={this.props.currentViewName} views={this.props.views} changeView={this.props.changeView}/>
             </div>
         );
     }
@@ -63,40 +181,83 @@ var SidebarNav = React.createClass({
 
 var SidebarProfile = React.createClass({
     render: function() {
-        return ( <div>Andrew Millspaugh</div> );
+        return ( 
+                <div id='sidebarProfile'>
+                <img src={this.props.user.photo}/>
+                <span>{this.props.user.name}</span>
+                <DropdownIcon/>
+                </div> 
+               );
     }
 });
 
 
 var Sidebar = React.createClass({
     render: function() {
-        return ( 
+        return (
                 <div id='sidebar'>
                     <img className='logo' src='img/logo.png' alt='Alveare Logo'></img>
-                    <SidebarNav/>
-                    <SidebarProfile/>
-                </div> 
+                    <SidebarNav changeView={this.props.changeView} views={this.props.views} currentViewName={this.props.currentViewName}/>
+                    <SidebarProfile user={this.props.user}/>
+                </div>
                );
     }
 });
 
 var App = React.createClass({
+    getInitialState: function() {
+        return {
+            organization: {name: 'alveare'},
+            project: {name: 'api'},
+            user: {
+                name: 'Andrew Millspaugh',
+                photo: 'img/andrew.png'
+            },
+            views: [
+                { name: 'New/Waiting', element: <NewTicketView tickets={this.props.tickets}/>, icon: 'img/new-waiting-24px.svg', selected: true },
+                { name: 'In Progress', element: <div> In progress! </div>, icon: 'img/in-progress-24px.svg', selected: false },
+                { name: 'Completed', element: <div> Completed! </div>, icon: 'img/completed-24px.svg', selected: false }
+            ],
+            currentViewName: 'New/Waiting'
+        };
+    },
+    changeProject: function(organization, project) {
+        this.setState({ 
+            organization: organization,
+            project: project
+        });
+    },
+    changeView: function(viewName) {
+        this.setState({ currentViewName: viewName });
+    },
     render: function() {
-        return ( <div id='app'><Sidebar /><MainContent /></div> );
+        currentView = this.state.views.filter(function(view) { return view.name == this.state.currentViewName; }.bind(this))[0];
+        return ( 
+                <div id='app'>
+                <Sidebar 
+                    organization={this.state.organization} 
+                    project={this.state.project} 
+                    user={this.state.user} 
+                    views={this.state.views} 
+                    currentViewName={this.state.currentViewName}
+                    changeView={this.changeView}
+                />
+                {currentView.element}
+                </div> 
+               );
     }
 });
 
 var TICKETS = [
-    {title: 'Fix this one thing', date: '1', skillsRequired: 'Python', comments: ['Foo','Bar']},
-    {title: 'Fix this other thing', date: '2', skillsRequired: 'SQLAlchemy', comments: ['Foo','Bar', 'Baz']},
-    {title: 'What the fuck you doin?', date: '3', skillsRequired: 'unittest', comments: ['Foo']},
-    {title: 'Get rid of all this shit', date: '5', skillsRequired: 'State Machines', comments: ['Foo','Bar','Baz','Qux']},
-    {title: 'Do my bidding', date: '4', skillsRequired: 'unittest', comments: ['Foo','Bar']},
-    {title: 'Do my bidding', date: '6', skillsRequired: 'unittest', comments: ['Foo','Bar']},
-    {title: 'Do my bidding', date: '7', skillsRequired: 'unittest', comments: ['Foo','Bar']},
-    {title: 'Do my bidding', date: '8', skillsRequired: 'unittest', comments: ['Foo','Bar']},
-    {title: 'Do my bidding', date: '9', skillsRequired: 'unittest', comments: ['Foo','Bar']},
-    {title: 'Do my bidding', date: '10', skillsRequired: 'unittest', comments: ['Foo','Bar']},
+    {title: 'Abstract out state machine event resource creation', date: '1', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
+    {title: 'For all ORM relationships that reference a table with composite foreign key, combine those keys into one relationship.', date: '2', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar', 'Baz']},
+    {title: 'Add permission checking to alveare REST object creation and update methods.', date: '3', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo']},
+    {title: 'Refactor auction ORM model to remove organization helper relationship.', date: '5', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar','Baz','Qux']},
+    {title: 'Change bank account model initialization method to take a generic owner parameter.', date: '4', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
+    {title: 'Do this one thing and then the other thing so we have more things to do.', date: '6', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
+    {title: 'Delete every line of code from the codebase, destroy everybody\'s computers, and delete our github accounts.', date: '7', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
+    {title: 'Play with the quadcopter for 10-15 hours.', date: '8', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
+    {title: 'Build some really cool software, get a bunch of people to use it, and make a billion dollars.', date: '9', skillsRequired: 'Python | SQLAlchemy | Finite State Machine', comments: ['Foo','Bar']},
 ];
 
-React.render(<App />, document.body);
+React.render(<App tickets={TICKETS}/>, document.body);
