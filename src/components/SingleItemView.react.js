a9c084b116f9dcff92f29fc4ffebec47d4df8606
Andrew Millspaugh <andrew@alveare.io>
2015-08-23 00:20:32 -0700

    Got buttons working for another state

--- src/components/SingleItemView.react.js
+++ src/components/SingleItemView.react.js
@@ -4,6 +4,8 @@ var TicketStore = require('../stores/TicketStore');
 var RebaseActions = require('../actions/RebaseActions');
 var Icons = require('../components/RebaseIcons.react');
 var handleScrollShadows = require('../utils/Style').handleScrollShadows;
+var ViewConstants = require('../constants/ViewConstants');
+var ticketTypes = ViewConstants.ticketTypes;
 
 var SingleItemView = React.createClass({
     render: function() {
@@ -140,15 +142,31 @@ var CommentList = React.createClass({
 
 var ItemHeader = React.createClass({
     render: function() {
+        var buttons = [];
+        var makeButton = function(props) {
+            return <button onClick={props.onClick} className={props.className}>{props.text}</button>;
+        }
+        switch (this.props.ticket.type) {
+            case ticketTypes.NEW:
+                buttons = this.props.currentRole.type == 'developer' ?  [] : [{ onClick: this.props.buttonAction, text: 'Find Talent' }];
+            break;
+            case ticketTypes.OFFERED:
+                buttons = this.props.currentRole.type == 'developer' ?  [{ onClick: this.props.buttonAction, text: 'Bid Now' }] : [];
+            break;
+            case ticketTypes.IN_PROGRESS:
+                buttons = this.props.currentRole.type == 'developer' ? 
+                    [{onClick: this.props.buttonAction, text: 'Finish' },
+                        { onClick: this.props.buttonAction, text: 'Mark Blocked', className: 'danger' }] : [];
+            break;
+            case ticketTypes.COMPLETED: break;
+        }
         return (
             <div id='itemHeader'>
                 <div onClick={this.props.backAction} className='backButton'>
                     <Icons.Dropback/>
                 </div>
+                { buttons.map(makeButton) }
                 <span>{this.props.title}</span>
-                <button onClick={this.props.buttonAction}>
-                    {this.props.currentRole.type == 'developer' ? 'Bid Now' : 'Find Talent'}
-                </button>
             </div>
         );
     }
