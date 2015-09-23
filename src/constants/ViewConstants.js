c63cfa2a36a00fae1cd02953d0e6f0038c9c4a42
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-09-23 15:23:53 -0700

    Some minor visual changes, including new main icons

--- src/constants/ViewConstants.js
+++ src/constants/ViewConstants.js
@@ -19,7 +19,7 @@ CONTRACTOR_VIEWS[VIEW_TYPES.COMPLETED] = {name: 'Completed', icon: 'img/complete
 MANAGER_VIEWS[VIEW_TYPES.NEW] = { name: 'New', icon: 'img/new-24px.svg', type: VIEW_TYPES.NEW },
 MANAGER_VIEWS[VIEW_TYPES.OFFERED] = { name: 'Waiting', icon: 'img/waiting-24px.svg', type: VIEW_TYPES.OFFERED },
 MANAGER_VIEWS[VIEW_TYPES.IN_PROGRESS] = { name: 'In Progress', icon: 'img/in-progress-24px.svg', type: VIEW_TYPES.IN_PROGRESS },
-MANAGER_VIEWS[VIEW_TYPES.COMPLETED] = { name: 'Completed', icon: 'img/completed-24px.svg', type: VIEW_TYPES.COMPLETED },
+MANAGER_VIEWS[VIEW_TYPES.COMPLETED] = { name: 'Completed', icon: 'img/completed-manager-24px.svg', type: VIEW_TYPES.COMPLETED },
 
 module.exports = {
     ViewTypes: VIEW_TYPES,
