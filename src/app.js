1398b264047f27e1e9dbdfdadd71e567b9b636ca
Raphael <raphael@joinrebase.com>
2015-09-29 14:28:17 -0700

    Add D3.js to bundle

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var RebaseApp = require('./components/RebaseApp.react');
+var D3 = require('d3');
 ReactDOM.render(<RebaseApp />, document.getElementById('react-app'));
