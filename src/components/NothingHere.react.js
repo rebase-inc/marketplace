3f65d08184ac0058b16633fd061cd978f55220d5
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-09-29 01:07:23 -0700

    Added animation to appearance of NothingHere

--- src/components/NothingHere.react.js
+++ src/components/NothingHere.react.js
@@ -1,10 +1,18 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var NothingHere = React.createClass({
+    getInitialState: () => ({mounted: false}),
+    componentDidMount: function() {
+        setTimeout(function() {
+            !!this.isMounted() ?  this.setState({ mounted: true }) : null;
+        }.bind(this), 50);
+    },
     render: function() {
         return (
             <div id='nothingHere'>
-                <div>
+                <div id='opacityMask' className={!!this.state.mounted ? 'fade-away' : ''} />
+                <div id='content'>
                     <h3>In order to get some work done, you first need some tasks</h3>
                     <button>Import GitHub Project</button>
                     <span>OR</span>
