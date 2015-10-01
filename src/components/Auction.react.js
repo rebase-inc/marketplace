3328d3235aabfbfd2a67fb8c0a003083ba4a2e4e
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-10-01 12:19:00 -0700

    Made whole auction clickable

--- src/components/Auction.react.js
+++ src/components/Auction.react.js
@@ -17,14 +17,14 @@ var Auction = React.createClass({
     },
     render: function() {
         return (
-            <tr className='auction'>
-                { this.props.currentRole.type == 'manager' ?  
-                    <FindTalentPanel onClick={this.props.selectAuction.bind(null, this.props.auction.id)} /> :
+            <tr className='auction' onClick={this.props.selectAuction.bind(null, this.props.auction.id)}>
+                { this.props.currentRole.type == 'manager' ?
+                    <FindTalentPanel /> :
                     <ProjectInfoPanel ticket={this.props.auction.ticket} /> }
                 <td className='titlePanel'>{this.props.auction.ticket.title}</td>
                 { this.props.currentRole.type == 'manager' ? <td className='talentOverviewPanel'><Icons.FindTalentOverview auction={this.props.auction}/></td> : null }
                 <td className='skillsRequiredPanel'>{this.props.auction.ticket.skillsRequired}</td>
-                <td className='commentsPanel' onClick={this.props.selectAuction.bind(null, this.props.auction.id)}>
+                <td className='commentsPanel'>
                     <Icons.Comment/>
                     <span>{this.props.auction.ticket.comments.length} Comments</span>
                 </td>
