d74def20def44d184c9f4717c85f4feea81c25f4
Raphael <raphael@joinrebase.com>
2015-10-02 12:06:05 -0700

    Refactor GithubAction and GithubStore to use utility functions

--- src/actions/GithubActions.js
+++ src/actions/GithubActions.js
@@ -1,39 +1,8 @@
-var Dispatcher = require('../dispatcher/RebaseAppDispatcher');
+var Action = require('../utils/Action');
 var ActionConstants = require('../constants/ActionConstants');
-var RequestConstants = require('../constants/RequestConstants');
 var Api = require('../utils/Api');
 
 module.exports = {
-    getAccounts: function() {
-        function responseAction(response, status) {
-            Dispatcher.handleRequestAction({
-                type: ActionConstants.GET_GITHUB_ACCOUNTS,
-                status: status,
-                response: response
-            });
-        };
-        function pendingAction() {
-            Dispatcher.handleRequestAction({
-                type: ActionConstants.GET_GITHUB_ACCOUNTS,
-                status: RequestConstants.PENDING,
-            });
-        };
-        Api.getGithubAccounts(responseAction, pendingAction);
-    },
-    importRepos: function(selectedRepos) {
-        function responseAction(response, status) {
-            Dispatcher.handleRequestAction({
-                type: ActionConstants.IMPORT_GITHUB_REPOS,
-                status: status,
-                response: response
-            });
-        };
-        function pendingAction() {
-            Dispatcher.handleRequestAction({
-                type: ActionConstants.IMPORT_GITHUB_REPOS,
-                status: RequestConstants.PENDING,
-            });
-        };
-        Api.importGithubRepos(selectedRepos, responseAction, pendingAction);
-    }
+    getAccounts: Action.create(ActionConstants.GET_GITHUB_ACCOUNTS,   Api.getGithubAccounts),
+    importRepos: Action.create(ActionConstants.IMPORT_GITHUB_REPOS,   Api.importGithubRepos),
 };
--- src/stores/GithubStore.js
+++ src/stores/GithubStore.js
@@ -1,71 +1,45 @@
 var _ = require('underscore');
 
-var Dispatcher = require('../dispatcher/RebaseAppDispatcher');
-var EventEmitter = require('events').EventEmitter;
+var Store = require('../utils/Store');
 var ActionConstants = require('../constants/ActionConstants');
 var RequestConstants = require('../constants/RequestConstants');
 var ProjectResource = require('../stores/ProjectStore');
 
 //Define initial data points
-var _loading = true;
-var _allAccounts = [];
-
-var GithubStore = _.extend({}, EventEmitter.prototype, {
-    getState: function() {
-        return {
-            allAccounts: _allAccounts,
-            loading: _loading,
-        };
-    },
-    emitChange: function(action_type) { this.emit('change', action_type); },
-    addChangeListener: function(callback) { this.on('change', callback); },
-    removeChangeListener: function(callback) { this.removeListener('change', callback); }
-});
-
-// Register callback with Dispatcher
-Dispatcher.register(function(payload) {
-    var action = payload.action;
-    switch(action.type) {
-        case ActionConstants.GET_GITHUB_ACCOUNTS: handleNewAccountData(action); break;
-        case ActionConstants.IMPORT_GITHUB_REPOS: handleImportRepos(action); break;
-        default: return true;
-    }
-
-    // If action was responded to, emit change event
-    GithubStore.emitChange(action.type);
-    return true;
-});
-
-function handleNewAccountData(action) {
-    switch (action.status) {
-        case RequestConstants.PENDING: _loading = true; break;
-        case RequestConstants.TIMEOUT: _loading = false; console.warn(action.response); break;
-        case RequestConstants.ERROR: _loading = false; console.warn(action.response); break;
-        case null: _loading = false; console.warn('Undefined data!'); break;
-        case undefined: _loading = false; console.warn('Undefined data!'); break;
-        default: {
-            _loading = false;
-            _allAccounts = action.response.github_accounts;
-        }
-
-    }
+var _githubData = {
+    loading:        true,
+    allAccounts:    [],
 }
 
-function handleImportRepos(action) {
-    switch (action.status) {
-        case RequestConstants.PENDING: _loading = true; break;
-        case RequestConstants.TIMEOUT: _loading = false; console.warn(action.response); break;
-        case RequestConstants.ERROR: _loading = false; console.warn(action.response); break;
-        case null: _loading = false; console.warn('Undefined data!'); break;
-        case undefined: _loading = false; console.warn('Undefined data!'); break;
-        default: {
-            _loading = false;
-            var _updated_repos = action.response.repos; // TODO update repository store once built
-            var _updated_orgs = action.response.orgs; // TODO update organization store once built
-            var _updated_projects = action.response.projects;
-            ProjectResource.update(action.response.projects);
-        }
-    }
-}
+var GithubStore = Store.newStore(function() {
+    return _githubData;
+});
+
+function successGetAccounts(action) {
+    _githubData.loading = false;
+    _githubData.allAccounts = action.response.github_accounts;
+};
+
+Store.registerDispatcher(
+    GithubStore,
+    ActionConstants.GET_GITHUB_ACCOUNTS,
+    successGetAccounts,
+    Store.defaultPendingAndErrorHandler.bind(_githubData)
+);
+
+function successImportRepos(action) {
+    _githubData.loading = false;
+    var _updated_repos = action.response.repos; // TODO update repository store once built
+    var _updated_orgs = action.response.orgs; // TODO update organization store once built
+    var _updated_projects = action.response.projects;
+    ProjectResource.update(action.response.projects);
+};
+
+Store.registerDispatcher(
+    GithubStore,
+    ActionConstants.IMPORT_GITHUB_REPOS,
+    successImportRepos,
+    Store.defaultPendingAndErrorHandler.bind(_githubData)
+);
 
 module.exports = GithubStore;
