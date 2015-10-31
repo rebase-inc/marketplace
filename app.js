8e2967be723eeb65196e6756a778e06ac4b75484
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-10-30 17:21:09 -0700

    Removed old app initialization

--- app.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var RebaseApp = require('./components/RebaseApp.react');
-var D3 = require('d3');
-
-ReactDOM.render(<RebaseApp />, document.getElementById('react-app'));
