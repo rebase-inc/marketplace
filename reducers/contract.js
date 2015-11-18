12eaf1c4791ffb0f61d0d29a7ddabd840ac11b09
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-11-18 15:21:43 -0800

    Dealt with state switching after bidding on auction

--- reducers/contract.js
+++ reducers/contract.js
@@ -11,6 +11,15 @@ export default function contract(contract = initialContract, action) {
                     return { isFetching: false, id: action.response.contractId }
             }
         }
+        case ActionConstants.BID_ON_AUCTION: {
+            switch (action.status) {
+                case PENDING: return Object.assign({}, contract, { isFetching: true }); break;
+                case ERROR: return Object.assign({}, contract, { isFetching: false }); break;
+                case SUCCESS:
+                    return { isFetching: false, id: action.response.auction.bids[0].contract.id };
+                    break;
+            }
+        }
         case ActionConstants.CREATE_AUCTION: {
             switch (action.status) {
                 case PENDING: return Object.assign({}, contract, { isFetching: true }); break;
