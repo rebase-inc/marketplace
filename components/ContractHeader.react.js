72972b19162a10abe64e7c21b0372b5840214580
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-11-19 14:48:56 -0800

    Restyle details panel and fix ContractHeader colors

--- components/ContractHeader.react.js
+++ components/ContractHeader.react.js
@@ -35,7 +35,7 @@ export class InProgressContractHeader extends Component {
     render() {
         const { role, contract, unselect, toggleDetails, actions } = this.props;
         return (
-            <TicketHeader title={contract.ticket.title} unselect={unselect} toggleDetails={toggleDetails}>
+            <TicketHeader notification title={contract.ticket.title} unselect={unselect} toggleDetails={toggleDetails}>
                 { role.type == 'contractor' ? <button onClick={actions.openSubmitWorkModal} key='finished'>Finished</button> : null }
                 { role.type == 'contractor' ? <button data-alert onClick={actions.openBlockWorkModal} key='blocked'>Blocked</button>: null }
             </TicketHeader>
