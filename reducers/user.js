548033a965835e45aec0f0813957956e7886d21f
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-11-19 16:32:35 -0800

    Fix bug that makes it looked like you're logged out when you switch projects

--- reducers/user.js
+++ reducers/user.js
@@ -4,6 +4,7 @@ import { PENDING, SUCCESS, UNAUTHORIZED, ERROR } from '../constants/RequestConst
 let initialUser = { email: null, isFetching: false, error: null };
 
 export default function user(user = initialUser, action) {
+    console.log('handling action ', action);
     switch (action.type) {
         case ActionConstants.LOGIN: return handleNewUserData(action.status, user, action.response.user); break;
         case ActionConstants.UPDATE_PROFILE: return handleNewUserData(action.status, user, action.response.user); break;
@@ -14,9 +15,9 @@ export default function user(user = initialUser, action) {
 
 function handleNewUserData(requestStatus, oldUser, newUser) {
     switch (requestStatus) {
-        case PENDING: return Object.assign({}, initialUser, { isFetching: true }); break;
+        case PENDING: return Object.assign({}, oldUser, { isFetching: true }); break;
         case ERROR: return Object.assign({}, oldUser, { isFetching: false }); break;
-        case UNAUTHORIZED: return Object.assign({}, initialUser, { error: 'Invalid credentials' }); break;
+        case UNAUTHORIZED: return Object.assign({}, oldUser, { error: 'Invalid credentials' }); break;
         case SUCCESS:
             const { id, email, current_role, first_name, last_name, photo } = newUser;
             const newUserData = { id, email, first_name, last_name, photo, current_role: {id: current_role.id} };
