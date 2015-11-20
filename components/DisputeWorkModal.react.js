682efcedf42f94e52cfed4179d18022b17f32770
Raphael <raphael@joinrebase.com>
2015-11-20 12:36:05 -0800

    Fix for missing prop in DisputeWorkModal

--- components/DisputeWorkModal.react.js
+++ components/DisputeWorkModal.react.js
@@ -18,7 +18,7 @@ export default class DisputeWorkModal extends Component {
     render() {
         const { close, disputeWork } = this.props;
         return (
-            <ModalContainer toggleModal={close}>
+            <ModalContainer close={close}>
                 <h3>Dispute Developer Work</h3>
                 <textarea required ref='comment' placeholder="Leave a comment explaining why the work isn't yet complete."/>
                 <button className='warning' onClick={() => { disputeWork(this.state.text); close(); }}>Dispute</button>
