7b9cbf3ca94aa3630c60e18f2084801b702ad467
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-11-20 18:18:26 -0800

    Added markdown to comment rendering

--- components/Comment.react.js
+++ components/Comment.react.js
@@ -4,9 +4,31 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import ProfilePicture from './ProfilePicture.react';
 import {humanReadableDate} from '../utils/date.js';
 
+import marked from 'marked';
+import highlight from 'highlight.js';
+import 'highlight.js/styles/solarized_dark.css';
+
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    highlight: code => highlight.highlightAuto(code).value,
+});
+
+
+
 export default class Comment extends Component {
     static propTypes = {
        comment: PropTypes.object.isRequired,
+    }
+    rawMarkup() {
+        var rawMarkup = marked(this.props.comment.content.toString(), {sanitize: true});
+        return { __html: rawMarkup };
     }
     render() {
         const { comment } = this.props;
@@ -18,7 +40,7 @@ export default class Comment extends Component {
                 <div className='content'>
                     <div className='name'>{comment.user.first_name + ' ' + comment.user.last_name}</div>
                     <div className='date'>{humanReadableDate(comment.created)}</div>
-                    <div className='text'>{comment.content}</div>
+                    <div className='text' dangerouslySetInnerHTML={this.rawMarkup()}/>
                 </div>
             </div>
         );
