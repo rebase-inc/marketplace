743cdcb9e3c8f742b0bbbcb06aa76f97051706e4
Raphael <raphael@joinrebase.com>
2015-12-08 16:24:34 -0800

    Change getContractComments and getReviewComments to pull comments from Work.blockages

--- utils/getters.js
+++ utils/getters.js
@@ -8,6 +8,7 @@ export const getContractWork = (contract) => contract.bid.work_offers[0].work;
 export const getContractComments = (contract) => {
     const comments = getContractTicket(contract).comments.concat(contract.bid.work_offers[0].work.comments);
     contract.bid.work_offers[0].work.mediations.forEach((m) => Array.prototype.push.apply(comments, m.comments));
+    contract.bid.work_offers[0].work.blockages.forEach((b) => Array.prototype.push.apply(comments, b.comments));
     return comments.sort((c1, c2) => new Date(c1.created) - new Date(c2.created));
 }
 
@@ -15,5 +16,6 @@ export const getReviewTicket = (review) => review.work.offer.ticket_snapshot.tic
 export const getReviewComments = (review) => {
     const comments = getReviewTicket(review).comments.concat(review.work.comments);
     review.work.mediations.forEach((m) => Array.prototype.push.apply(comments, m.comments));
+    review.work.blockages.forEach((b) => Array.prototype.push.apply(comments, b.comments));
     return comments.concat(review.comments).sort((c1, c2) => new Date(c1.created) - new Date(c2.created));
 }
