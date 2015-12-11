46f64ef80087f662ab63e87841a145a194e12771
Raphael <raphael@joinrebase.com>
2015-12-11 10:25:32 -0800

    Prepare react repo for Heroku app deployment

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+const TARGET=process.env.REBASE_HOST || 'http://localhost:5000'
+const PORT=process.env.PROXY_PORT || 3000;
+
 new WebpackDevServer(
     webpack(config), {
         publicPath: config.output.publicPath,
         hot: true,
-        proxy: { '/api/*': { target: 'http://localhost:5000', secure: true } },
+        proxy: { '/api/*': { target:TARGET, secure: true } },
         historyApiFallback: true,
         stats: { colors: true }
-    }).listen(3000, 'localhost', function (err) {
+    }).listen(PORT, 'localhost', function (err) {
         if (err) { console.log(err); }
-        console.log('Listening at localhost:3000');
+        console.log('Listening at localhost:'+PORT);
 });
---
+++ setup.sh
@@ -0,0 +1,2 @@
+export REBASE_HOST='http://localhost:5000'
+export PROXY_PORT=3000
