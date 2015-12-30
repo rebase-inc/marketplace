23b42b8bc5480cf091d4a114cb4655621bd653aa
Raphael <raphael@joinrebase.com>
2015-12-30 11:59:00 -0800

    Add polling option to web-dev-package
    This is get around a missing feature of vboxfs:
    it does not understand OS X's inotify events and that prevents
    the watchify of webpack from working.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var config = require('./webpack.config');
 
 // web_1 is a /etc/hosts entry pointing at a container hosting the backend (api).
 // web_1 is setup by a link in <repos>/api/docker-compose.yml
-const TARGET=process.env.REBASE_HOST || 'http://web_1:5000' || 'http://localhost:5000'
+const TARGET='http://web_1:5000' || 'http://localhost:5000'
 const PORT=process.env.PORT || 3000;
 
 new WebpackDevServer(
@@ -13,7 +13,11 @@ new WebpackDevServer(
         hot: true,
         proxy: { '/api/*': { target:TARGET, secure: true } },
         historyApiFallback: true,
-        stats: { colors: true }
+        stats: { colors: true },
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        },
     }).listen(PORT, function (err) {
         if (err) { console.log(err); }
         console.log('Listening at 0.0.0.0:'+PORT);
