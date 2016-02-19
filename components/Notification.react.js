6c74108d4d36bb21d32833a78fdbe57d8a6a75bf
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-02-19 09:32:48 +0700

    Added some animation to make the notification more noticeable

--- components/Notification.react.js
+++ components/Notification.react.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Notification extends Component {
     constructor(props, context) {
         super(props, context);
-        this._timer = setInterval(this.props.clear, 10000);
+        this._timer = setInterval(this.props.clear, 3000);
         this.componentWillUnmount = this.componentWillUnmount.bind(this);
     }
     componentWillUnmount() {
