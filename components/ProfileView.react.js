2735a287b41c3cb626c3451c428e0c28e7bfcd43
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-06-12 21:21:55 -0700

    Change GithubAccountTag and ProfileView to accomodate new shape of Github Account serialized data

--- components/ProfileView.react.js
+++ components/ProfileView.react.js
@@ -47,7 +47,7 @@ export default class ProfileView extends Component {
                     <InputField value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} loading={user.name != this.state.name}/>
                     <InputField value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} loading={user.email != this.state.email}/>
                     <div id='githubAccounts'>
-                        { user.github_accounts.map(ga => <GithubAccountTag account={ga} key={ga.id} />) }
+                        { user.github_accounts.map(ga => <GithubAccountTag account={ga} key={ga.user_id} />) }
                         <span onClick={() => window.location.replace('/api/v1/github')}>{ 'Add Github Account' }</span>
                     </div>
                     <div id='sshKeys'>
