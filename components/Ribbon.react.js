08a326a0fef9b6a3e455f1e6cdaba8a77f7fc6c3
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-10-13 21:18:03 -0700

    Cleanup tech strings before rendering Ribbons

--- components/Ribbon.react.js
+++ components/Ribbon.react.js
@@ -1,7 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+const table = [
+    ["_overall", "Overall"],
+    [".__language__",          "/Language"],
+    [".__standard_library__",  "/Standard Library"],
+    [".__third_party__.",       "/"],
+    [".__third_party__",       "/3rd Party"]
+];
+
+
+function replace(str) {
+    return table.reduce( (before, sub) => before.replace(sub[0], sub[1]), str);
+}
+
 export default class Ribbon extends Component {
+
     static propTypes = {
         tech: PropTypes.string.isRequired,
         ranking: PropTypes.number.isRequired
@@ -9,9 +23,10 @@ export default class Ribbon extends Component {
 
     render() {
         let { tech, ranking } = this.props;
+        let modified_tech = replace(tech);
         let modified_ranking = Math.ceil(100*ranking);
         return (
-            <button className='ribbon'>{tech}:  Top {modified_ranking}%</button>
+            <button className='ribbon'>{modified_tech}:  Top {modified_ranking}%</button>
         );
     }
 };
