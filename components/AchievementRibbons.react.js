50139618a23d00aaaf2e6173f6ca3dd2ea11c452
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-10-14 12:28:12 -0700

    Add sorting to ribbons

--- components/AchievementRibbons.react.js
+++ components/AchievementRibbons.react.js
@@ -4,6 +4,40 @@ import ReactDOM from 'react-dom';
 import Ribbon from './Ribbon.react';
 
 
+const table = [
+    ["_overall",                "Overall"],
+    [".__language__",           "/Language"],
+    [".__standard_library__",   "/Standard Library"],
+    [".__third_party__.",       "/"],
+    [".__third_party__",        "/3rd Party"]
+];
+
+
+function replace(str) {
+    return table.reduce( (before, sub) => before.replace(sub[0], sub[1]), str);
+}
+
+function renameSkill(skill) {
+    return [replace(skill[0]), skill[1]];
+}
+
+function isOverall(skill) {
+    return skill[0] == "Overall";
+}
+
+function isNotOverall(skill) {
+    return skill[0] != "Overall";
+}
+
+function byTech(skillA, skillB) {
+    if (skillA[0] > skillB[0]) {
+        return 1;
+    } else if (skillA[0] < skillB[0]) {
+        return -1;
+    }
+    return 0;
+}
+
 export default class AchievementRibbons extends Component {
     static propTypes = {
         skills: PropTypes.array.isRequired,
@@ -11,10 +45,28 @@ export default class AchievementRibbons extends Component {
 
     render() {
         let { skills } = this.props;
+        // 1 let's rename the technologies
+        // 2 extract Overall, so we can list it first
+        // 3 sort the remainder of the skills by technologies name
+        // 4 prepend Overall to the remainer
+
+        // 1
+        let renamed_skills = skills.map(renameSkill);
+
+        // 2
+        let overall = renamed_skills.find(isOverall);
+
+        // 3
+        let remainder = renamed_skills.filter(isNotOverall).sort(byTech);
+        console.log('remainder: %o', remainder);
+
+        // 4
+        let result = [ overall, ...remainder ];
+
         return (
             <div className='achievement-ribbons'>
                 {
-                    skills.map( entry => <Ribbon tech={entry[0]} ranking={entry[1]} /> )
+                    result.map( entry => <Ribbon tech={entry[0]} ranking={entry[1]} /> )
                 }
             </div>
         );
--- components/Ribbon.react.js
+++ components/Ribbon.react.js
@@ -1,18 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-const table = [
-    ["_overall", "Overall"],
-    [".__language__",          "/Language"],
-    [".__standard_library__",  "/Standard Library"],
-    [".__third_party__.",       "/"],
-    [".__third_party__",       "/3rd Party"]
-];
-
-
-function replace(str) {
-    return table.reduce( (before, sub) => before.replace(sub[0], sub[1]), str);
-}
 
 export default class Ribbon extends Component {
 
@@ -23,10 +11,11 @@ export default class Ribbon extends Component {
 
     render() {
         let { tech, ranking } = this.props;
-        let modified_tech = replace(tech);
         let modified_ranking = Math.ceil(100*ranking);
         return (
-            <button className='ribbon'>{modified_tech}:  Top {modified_ranking}%</button>
+            <button className='ribbon'>{tech}:  Top {modified_ranking}%</button>
         );
     }
 };
+
+
